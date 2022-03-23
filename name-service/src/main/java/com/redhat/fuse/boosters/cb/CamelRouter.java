cc6b3f325a0a2147f990b28e866861fdbe285079
jbride <jbride2001@yahoo.com>
2022-03-23 16:11:20 -0600

    backported JdbcOrphanLockAwareIdempotentRepository from camel-3.15.0 to camel-2.23.2

--- name-service/src/main/java/com/redhat/fuse/boosters/cb/CamelRouter.java
+++ name-service/src/main/java/com/redhat/fuse/boosters/cb/CamelRouter.java
@@ -3,13 +3,14 @@ package com.redhat.fuse.boosters.cb;
 import javax.sql.DataSource;
 
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.impl.DefaultCamelContext;
 import org.apache.camel.model.rest.RestBindingMode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 // Not available in Camel 2.23..2  (Fuse 7.10)
-// import org.apache.camel.processor.idempotent.jdbc.JdbcOrphanLockAwareIdempotentRepository;
+import org.apache.camel.processor.idempotent.jdbc.JdbcOrphanLockAwareIdempotentRepository;
 
 import org.apache.camel.processor.idempotent.jdbc.JdbcMessageIdRepository;
 import org.apache.camel.spi.IdempotentRepository;
@@ -26,7 +27,8 @@ public class CamelRouter extends RouteBuilder {
 
     @Bean(name="inProgressRepo")
     public IdempotentRepository inProgressRepo() {
-        IdempotentRepository jRepo = new JdbcMessageIdRepository(dataSource, "myProcessor");
+        IdempotentRepository jRepo = new JdbcOrphanLockAwareIdempotentRepository(dataSource, "myProcessor", new DefaultCamelContext());
+        //IdempotentRepository jRepo = new JdbcMessageIdRepository(dataSource, "myProcessor");
         return jRepo;
     }
 
@@ -34,24 +36,11 @@ public class CamelRouter extends RouteBuilder {
     @Override
     public void configure() throws Exception {
 
-        // @formatter:off
-        restConfiguration()
-            .component("servlet")
-            .bindingMode(RestBindingMode.json);
-        
-        rest("/name").description("Name REST service")
-            .consumes("application/json")
-            .produces("application/json")
-
-            .get().description("Generate a Name").outType(Name.class)
-                .responseMessage().code(200).endResponseMessage()
-                .to("bean:nameService?method=getName");
-        // @formatter:on
-        
         //from("file:///tmp/na/input?delay=1000&autoCreate=true&delete=false")
         from("file:///tmp/na/input?inProgressRepository=#inProgressRepo&delay=1000&autoCreate=true&delete=false")
             .routeId("direct:fileConsumer")
             .log("file = ${header.CamelFileName}}")
+            .delay(15000)
             .end();
     }
 
